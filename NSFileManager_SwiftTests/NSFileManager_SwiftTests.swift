e30fbb1c5febd28d8d8ccc8b6169c4f1be72b0ee
Rama Chandra <ram2chitti@Rama-Chandras-MacBook.local>
2014-10-13 15:40:00 +0100

    Initial Commit

---
+++ NSFileManager_SwiftTests/NSFileManager_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  NSFileManager_SwiftTests.swift
+//  NSFileManager_SwiftTests
+//
+//  Created by Rama Chandra on 13/10/2014.
+//  Copyright (c) 2014 Rama Chandra. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class NSFileManager_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
